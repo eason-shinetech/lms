d87dd65f5e84ca2b5e3cd972948fa84e7df07ddc
eason-shinetech <zhaost@shinetechsoftware.com>
2025-03-31 22:40:14 +0800

    Chapter detail

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,5 +1,23 @@
-const CourseDetail = () => {
-  return <div>Course Detail</div>;
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseDetail = async ({
+  params,
+}: {
+  params: Promise<{ courseId: string }>;
+}) => {
+  const { courseId } = await params;
+  const course = await db.course.findUnique({
+    where: { id: courseId },
+    include: {
+      chapters: {
+        where: { isPublished: true },
+        orderBy: { position: "asc" },
+      },
+    },
+  });
+  if (!course) return redirect("/");
+  return redirect(`/courses/${courseId}/chapters/${course.chapters[0].id}`);
 };
 
 export default CourseDetail;
